c58099156d12e36574b47dc2986b1fc44489abf9
Denis Iliukhin <denisiums@gmail.com>
2022-06-04 13:18:49 +0200

    game time loop

--- src/ts/models/game.ts
+++ src/ts/models/game.ts
@@ -1,6 +1,10 @@
 export class Game {
     ended = false;
+    time: number = 0;
+    dt: number = 10; // 10ms
+
     previousTimeStamp: number = 0;
+    accumulator: number = 0;
 
     constructor() {
 
@@ -21,17 +25,36 @@ export class Game {
         this.ended = true;
     }
 
-    private step(timestamp: number) {
+    private step(timestamp: number /* time from the start */) {
         console.log('timestamp: ', timestamp);
 
         if (this.ended) {
             return;
         }
 
+        const frameTime: number = timestamp - this.previousTimeStamp;
+        console.log('frameTime: ', frameTime);
+
+        this.accumulator = this.accumulator + frameTime;
+
+
+        while (this.accumulator >= this.dt) {
+            this.updateState(this.time, this.dt);
+
+            this.accumulator -= this.dt;
+            this.time += this.dt;
+        }
 
 
 
+
+
+        this.previousTimeStamp = timestamp;
         requestAnimationFrame(this.step.bind(this));
+    }
+
+    private updateState(time: number, dt: number) {
+        console.log('update state: ', time, dt);
     }
 
     private initialize() {
