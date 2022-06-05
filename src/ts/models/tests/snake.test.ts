09adb1f349e653e26a48bf5c7980f400d56267a3
Denis Iliukhin <denisiums@gmail.com>
2022-06-05 14:00:22 +0200

    tests starts

---
+++ src/ts/models/tests/snake.test.ts
@@ -0,0 +1,8 @@
+import {Snake} from '../snake';
+
+describe('Snake', () => {
+    test('Should be generated in position 10, 10 with correct segments position', () => {
+        // actual test
+        expect(1).toBe(1);
+    });
+});
